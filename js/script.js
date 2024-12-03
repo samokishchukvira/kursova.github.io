5595603fd8c48641e63f97cb270a5192a57ed8f8
samokishchukvira <159585110+samokishchukvira@users.noreply.github.com>
2024-12-03 17:43:24 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -117,30 +117,48 @@ document.addEventListener('DOMContentLoaded', () => {
     })
     .then(classes => {
       const classesSection = document.querySelector('.classes-grid');
-
+      
       classes.forEach(classItem => {
         const classDiv = document.createElement('div');
         classDiv.classList.add('class-item');
-        classDiv.dataset.aos = 'fade-up'; // Додає анімацію
+        
+        classItem.icon.split(' ').forEach(cls => classDiv.classList.add(cls));
 
-        // Додаємо фонове зображення
-        classDiv.style.backgroundImage = `url(${classItem.image})`;
-        classDiv.style.backgroundSize = 'cover';
-        classDiv.style.backgroundPosition = 'center';
-
-        // Додаємо інформацію про клас
         const classInfo = `
           <div class="class-info">
             <h3>${classItem.title}</h3>
+            <p>${classItem.description}</p> <!-- Опис класу -->
             <span><i class="${classItem.icon}"></i></span>
           </div>
         `;
-
         classDiv.innerHTML = classInfo;
+
         classesSection.appendChild(classDiv);
       });
     })
     .catch(error => console.error('Error loading classes:', error));
+
+    fetch('json/banner.json')
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+      return response.json();
+    })
+    .then(banner => {
+      const bannerSection = document.querySelector('.banner-content');
+
+      const bannerContent = `
+        <h2 class="animation _anim-item">${banner.title}</h2>
+        <p class="animation-paragraph _anim-item">${banner.description}</p>
+        <a href="${banner.buttonLink}">
+          <button class="banner-button" data-aos="fade-up" data-aos-duration="3000">${banner.buttonText}</button>
+        </a>
+      `;
+
+      bannerSection.innerHTML = bannerContent;
+    })
+    .catch(error => console.error('Error loading banner:', error));
 });
 
 
