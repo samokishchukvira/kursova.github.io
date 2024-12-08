927902d85007ef0c0ef2420df5cd54928168f05b
samokishchukvira <159585110+samokishchukvira@users.noreply.github.com>
2024-12-08 21:34:53 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -36,20 +36,20 @@ document.addEventListener('DOMContentLoaded', () => {
     }
 
     contactSection.innerHTML = `
-      <h2 class="animation _anim-item">${data.title}</h2>
-      <p class="animation-paragraph _anim-item">${data.description}</p>
+      <h2>${data.title}</h2>
+      <p>${data.description}</p>
       
       <form class="contact-form">
         <div class="form-row">
           ${data.fields.map((field) => `
             <div class="form-group">
-              <label for="${field.id}" class="animation _anim-item">${field.label}</label>
+              <label for="${field.id}">${field.label}</label>
               <input type="${field.type}" id="${field.id}" placeholder="${field.placeholder}">
             </div>
           `).join('')}
         </div>
         <div class="form-group">
-          <label for="message" class="animation _anim-item">${data.message.label}</label>
+          <label for="message">${data.message.label}</label>
           <textarea id="message" placeholder="${data.message.placeholder}"></textarea>
         </div>
         <button type="submit" class="submit-button" data-aos="fade-up" data-aos-duration="1000">${data.button}</button>
