7372a34fe9a1073464b7b1feb0820ea673564023
samokishchukvira <159585110+samokishchukvira@users.noreply.github.com>
2024-12-10 11:11:19 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,29 +1,3 @@
-AOS.init();
-
-if (window.innerWidth > 350) {  
-    AOS.init({
-      disable: false,  
-      startEvent: 'DOMContentLoaded', 
-      initClassName: 'aos-init', 
-      animatedClassName: 'aos-animate', 
-      useClassNames: false, 
-      disableMutationObserver: false, 
-      debounceDelay: 50, 
-      throttleDelay: 99, 
-      offset: 120, 
-      delay: 5, 
-      duration: 400, 
-      easing: 'ease',
-      once: false, 
-      mirror: false, 
-      anchorPlacement: 'top-bottom'
-    });
-  } else {
-    AOS.init({
-      disable: true,  
-    });
-  }
-
 const nav = document.querySelector(".nav"),
 searchIcon = document.querySelector("#searchIcon"),
 navOpenBtn = document.querySelector(".navOpenBtn"),
